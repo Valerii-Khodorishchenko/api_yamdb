c8c0343630a31dbf891585e0a249d4541f43656a
ValeriiKhodorishchenko <Khodorishchenko.Valerii@yandex.ru>
2024-10-30 03:25:56 +0500

    Сreat Review and Comment models
    The basic structure of the project has been extended with the models
    "Overview" and "Comments". Migrations have been modified to take into
    account the new models.
    
    * Сreat Review and Comment models api_yamdb/reviews/models.py
    * modified: api_yamdb/reviews/migrations/0001_initial.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,9 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+#TODO удалить после сздания кастомной модели User
+from django.contrib.auth import get_user_model
 
+User = get_user_model()  #TODO удалить после сздания кастомной модели User
 
 class Category(models.Model):
     name = models.CharField('Название категории', max_length=256)
@@ -41,3 +45,57 @@ class Title(models.Model):
 
     def __str__(self):
         return self.name[:26]
+
+
+class Review(models.Model):
+    text = models.TextField('Текст отзыва')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        verbose_name='Автор отзыва'
+    )
+    score = models.PositiveSmallIntegerField(
+        'Рейтинг',
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    title = models.ForeignKey(
+        Title, on_delete=models.CASCADE,
+        verbose_name='Произведение'
+    )
+
+    class Meta:
+        verbose_name = 'отзыв'
+        verbose_name_plural = 'Отзывы'
+        ordering = ('-pub_date',)
+        default_related_name = 'reviews'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['author', 'title'],
+                name='unique_author_title'
+            )
+        ]
+
+    def __str__(self):
+        return f'Отзыв от {self.author} на {self.title}'
+
+
+class Comment(models.Model):
+    text = models.TextField('Текст комментария')
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE,
+        verbose_name='Автор комментария'
+    )
+    pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
+    review = models.ForeignKey(
+        Review, on_delete=models.CASCADE,
+        verbose_name='Отзыв'
+    )
+
+    class Meta:
+        verbose_name = 'комментарий'
+        verbose_name_plural = 'Комментарии'
+        ordering = ('-pub_date',)
+        default_related_name = 'comments'
+
+    def __str__(self):
+        return f'{self.author} прокомментировал {self.review}'
