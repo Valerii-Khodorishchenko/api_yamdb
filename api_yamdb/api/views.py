14474f42ddd3fb183cb823a3e0aafbfb70150197
Vladislav Ibraev <vladislavibraev@gmail.com>
2024-11-10 14:38:25 +0500

    fix code_confirmation

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -66,10 +66,9 @@ def signup(request):
         raise ValidationError(
             {'email': 'Пользователь с таким email уже зарегистрирован.'})
     else:
-        confirmation_code = get_random_code()
-        user.confirmation_code = confirmation_code
+        user.confirmation_code = get_random_code()
         user.save()
-        send_confirmation_code(user, confirmation_code)
+        send_confirmation_code(user, user.confirmation_code)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
